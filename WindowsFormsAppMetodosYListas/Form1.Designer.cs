b7d04ce62643f429c9399135636e5eec9c64d7f7
ZEPHYRUS 111 <ZEPHYRUS 111@DESKTOP-LMLUBQI>
2025-04-21 08:27:28 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsAppMetodosYListas/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsAppMetodosYListas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstElementos = new System.Windows.Forms.ListBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(106, 91);
+            this.txtElemento.Multiline = true;
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(211, 60);
+            this.txtElemento.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(409, 131);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(133, 40);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstElementos
+            // 
+            this.lstElementos.FormattingEnabled = true;
+            this.lstElementos.ItemHeight = 20;
+            this.lstElementos.Location = new System.Drawing.Point(106, 214);
+            this.lstElementos.Name = "lstElementos";
+            this.lstElementos.Size = new System.Drawing.Size(221, 104);
+            this.lstElementos.TabIndex = 2;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(416, 200);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(125, 43);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.lstElementos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtElemento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lstElementos;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ WindowsFormsAppMetodosYListas/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppMetodosYListas
+{
+    public partial class Form1: Form
+    {
+        // Lista para almacenar elementos 
+
+        private List<string> elementos = new List<string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Método para agregar elementos 
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nuevoElemento = txtElemento.Text;
+
+            // Verifica si el texto no está vacío y solo contiene letras
+            if (!string.IsNullOrWhiteSpace(nuevoElemento) && nuevoElemento.All(char.IsLetter))
+            {
+                elementos.Add(nuevoElemento);
+                ActualizarLista();
+                txtElemento.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingresa solo letras (sin números ni caracteres especiales).");
+            }
+        }
+
+        // Método para eliminar elementos seleccionados 
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+
+        {
+
+            if (lstElementos.SelectedItem != null)
+
+            {
+
+                elementos.Remove(lstElementos.SelectedItem.ToString());
+
+                ActualizarLista();
+
+            }
+
+            else
+
+            {
+
+                MessageBox.Show("Selecciona un elemento para eliminar.");
+
+            }
+
+        }
+
+
+
+        // Método para actualizar ListBox 
+
+        private void ActualizarLista()
+
+        {
+
+            lstElementos.DataSource = null;
+
+            lstElementos.DataSource = elementos;
+
+        }
+    }
+}
